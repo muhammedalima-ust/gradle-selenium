85e03914951d6e3ac6ea50210f3f633b2077292d
Muhammed Ali M A <310919@ust.com>
2026-07-08 23:31:06 +0530

    added develocity agreement

--- gradlerepo/settings.gradle.kts
+++ gradlerepo/settings.gradle.kts
@@ -11,5 +11,11 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        publishing.onlyIf { true }
+    }
+}
 rootProject.name = "gradlerepo"
