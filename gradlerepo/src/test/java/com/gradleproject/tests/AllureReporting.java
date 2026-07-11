d3c64e5137a660fddd399386d10740c6dda38e39
Muhammed Ali M A <310919@ust.com>
2026-07-11 08:41:05 +0530

    add log to allure reporting

--- gradlerepo/src/test/java/com/gradleproject/tests/AllureReporting.java
+++ gradlerepo/src/test/java/com/gradleproject/tests/AllureReporting.java
@@ -4,6 +4,8 @@ import io.qameta.allure.*;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,12 +18,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Feature("Reporting Insights")
 @Owner("SDET Trainee")
 class AllureReporting {
+    private static final Logger log =
+            LoggerFactory.getLogger(AllureReporting.class);
+
 
     @Test
     @Story("Categories")
     @Severity(SeverityLevel.CRITICAL)
     @Description("Categories split flaky,test and product")
     void categoriesPutGenericFlakyRules() throws IOException{
+        log.info("Test 1: Validating Allure Categories Configuration");
         String categories = Files.readString(Path.of("src/test/resources/allure/categories.json"));
 
         int flakyIndex = categories.indexOf("\"Flaky tests\"");
@@ -33,6 +39,8 @@ class AllureReporting {
         assertTrue(productDefectIndex > flakyIndex);
         assertTrue(categories.contains("\"flaky\": true"));
         assertTrue(categories.contains("timeout|stale element|connection reset"));
+        log.info("Allure categories.json validated successfully.");
+        
     }
 
     @Test
@@ -40,6 +48,7 @@ class AllureReporting {
     @Severity(SeverityLevel.CRITICAL)
     @Description("Should appear under Product defects")
     void productDefect() {
+        log.info("Test 2: Creating a Fake Product defect by assert 10==20");
         assertEquals(10, 20, "Incorrect cart total");
     }
 
@@ -48,6 +57,7 @@ class AllureReporting {
     @Severity(SeverityLevel.CRITICAL)
     @Description("Should appear under Test defects (broken)")
     void brokenDefect() {
+        log.info("Test 3: Creating a Fake Test defect checking null text length");
         String text = null;
         text.length();
     }
@@ -57,7 +67,8 @@ class AllureReporting {
     @Severity(SeverityLevel.CRITICAL)
     @Description("Should appear under Test defects (broken)")
     void brokenDefect1() {
-        throw new RuntimeException("ERROR");
+        log.info("Test 3: Creating a Fake Test defect By throwing any RunTimeException");
+        throw new RuntimeException("Runtime Exception Error");
     }
 
     @Test
