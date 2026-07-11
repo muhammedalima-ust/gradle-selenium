45c0a9a65ef5c7faa57e46ca8e3fce7a53d0b86d
Muhammed Ali M A <310919@ust.com>
2026-07-11 10:45:32 +0530

    Added Steps

--- gradlerepo/src/test/java/com/gradleproject/tests/AllureReporting.java
+++ gradlerepo/src/test/java/com/gradleproject/tests/AllureReporting.java
@@ -27,18 +27,22 @@ class AllureReporting {
     void categoriesPutGenericFlakyRules() throws IOException{
         Report.step("Test 1: Validating Allure Categories Configuration");
         String categories = Files.readString(Path.of("src/test/resources/allure/categories.json"));
-
+        Report.step("Category file is stored");
         int flakyIndex = categories.indexOf("\"Flaky tests\"");
         int testDefectIndex = categories.indexOf("\"Test defects (broken)\"");
         int productDefectIndex = categories.indexOf("\"Product defects\"");
-
+        Report.step("Index of Each fail categories is stored");
         assertTrue(flakyIndex >=0);
+        Report.pass("Flacky Category is inside the category is above 0");
         assertTrue(testDefectIndex > flakyIndex);
+        Report.pass("Flacky Category is above the Test Defect");
         assertTrue(productDefectIndex > flakyIndex);
+        Report.pass("Flacky Category is above the Product Defect");
         assertTrue(categories.contains("\"flaky\": true"));
+        Report.pass("Flaky Tag inside category is true");
         assertTrue(categories.contains("timeout|stale element|connection reset"));
-        Report.step("Allure categories.json validated successfully.");
-        
+        Report.pass("The Categories contain timeout|stale element|connection reset");
+        Report.pass("Allure categories.json validated successfully.");
     }
 
     @Test
@@ -47,6 +51,7 @@ class AllureReporting {
     @Description("Should appear under Product defects")
     void productDefect() {
         Report.step("Test 2: Creating a Fake Product defect by assert 10==20");
+        Report.fail("Intentiallly failed for allure report categorisation");
         assertEquals(10, 20, "Incorrect cart total");
     }
 
@@ -57,6 +62,9 @@ class AllureReporting {
     void brokenDefect() {
         Report.step("Test 3: Creating a Fake Test defect checking null text length");
         String text = null;
+        Report.step("Test is initialised with null");
+        Report.step("String length() method is used assert");
+        Report.fail("Intentiallly failed for allure report categorisation");
         text.length();
     }
 
@@ -66,6 +74,7 @@ class AllureReporting {
     @Description("Should appear under Test defects (broken)")
     void brokenDefect1() {
         Report.step("Test 3: Creating a Fake Test defect By throwing any RunTimeException");
+        Report.fail("Intentiallly throw error for allure report categorisation");
         throw new RuntimeException("Runtime Exception Error");
     }
 
