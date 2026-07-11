e387813141477cb9958033207a0164d11d5add77
Muhammed Ali M A <310919@ust.com>
2026-07-11 12:17:39 +0530

    Added Logs in OrderTestIT

--- gradlerepo/src/test/java/com/gradleproject/tests/OrderTestIT.java
+++ gradlerepo/src/test/java/com/gradleproject/tests/OrderTestIT.java
@@ -53,13 +53,13 @@ class OrderTestIT {
         Report.info("MySql Container JDBC URL      : {}", mySQL.getJdbcUrl());
         Report.info("MySql Container Database      : {}", mySQL.getDatabaseName());
 
-        Report.step("========== Flyway Migration Started ==========");
+        Report.step("Flyway Migration Started");
         Flyway.configure()
                 .dataSource(mySQL.getJdbcUrl(), mySQL.getUsername(), mySQL.getPassword())
                 .locations("classpath:db/migration")
                 .load()
                 .migrate();
-        Report.step("========== Flyway Migration Completed ==========");
+        Report.step("Flyway Migration Completed");
 
 
         repository = new OrderRepository(mySQL.getJdbcUrl(), mySQL.getUsername(), mySQL.getPassword());
@@ -72,7 +72,7 @@ class OrderTestIT {
     void reset(){
         Report.step("Resetting database...");
         repository.resetMutableTables();
-        Report.step("Database reset complete.");
+        Report.pass("Database reset complete.");
     }
 
     @Test
@@ -80,44 +80,50 @@ class OrderTestIT {
         void flywaySeedingReferenceDataButNoPerTestOrders(){
             Report.step("Test 1: Flyway Migration Testing Started");
             assertEquals(4,repository.referenceStatusCount());
-
+            Report.pass("Order Status DB is created and 4 row is there");
+            
             assertEquals(0,repository.count());
-            Report.step("Test 1: Completed");
-            Report.step("Tested Order Status DB have 4 Row and Retail Order DB Have 0 Rows : Test Passed");
+            Report.pass("Orders DB is created and data is empty");
+            Report.pass("Test 1:PASSED");
         }
         
     @Test
     void persistedBuilderDataAgainstIsolatedMysql(){
             Report.step("Test 2: Data Persisted Test for Factory");
             long id = factory.persisted(OrderBuilder.anOrder().qty(3));
-            
+            Report.step("Order is created and id is stored in variable");
             assertTrue(id>0);
+            Report.pass("Created id is positive value");
             assertEquals(1,repository.count());
-            Report.step("Test 2: Completed");
-            Report.step("Tested Order ID is greater than 0 and Retail Order DB have a row : Test Passed");
+            Report.pass("Order DB has 1 row");
+            Report.pass("Test 2:PASSED");
         }
         
     @Test
     void countsOnlyPersistedTestOrders(){
             Report.step("Test 3: Data Persisted Test for Factory is consistant");
             factory.persisted(OrderBuilder.anOrder());
+            Report.step("Order is created");
             factory.persisted(OrderBuilder.anOrder().sku("SKU-2").qty(2));
+            Report.step("Another Order is created");
             
             assertEquals(2,repository.count());
-            Report.step("Test 3: Completed");
-            Report.step("Tested the Retail Order DB Have 2 Row : Test Passed");
-        
+            Report.pass("Order DB has 2 row");
+            Report.pass("Test 3:PASSED");
     }
 
     @Test
     void resetMakesTestOrderIndependent(){
             Report.step("Test 4: Order Created is Exactly what we used");
             assertEquals(0,repository.count());
+            Report.pass("Order DB has 0 row");
             factory.persisted(OrderBuilder.anOrder().refunded());
+            Report.step("Order is created");
             
             assertEquals(1,repository.count());
+            Report.pass("Order DB has 1 row");
             assertEquals(1,repository.countByStatus("REFUNDED"));
-            Report.step("Test 4: Completed");
-            Report.step("Tested the retail order db is row is 1 and retail order db with filter status REFUNDED is 1 : Test Passed");
+            Report.pass("Order DB has 1 row ordr with status REFUNDED");
+            Report.pass("Test 4:PASSED");
     }
 }
